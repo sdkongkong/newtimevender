37cb3e8b5d50e7e6ac7bb05aaf97a9264f63cb1f
kevin song <shirley.gow@team.telstra.com>
2018-08-20 21:33:57 +1000

    empty project

---
+++ app/src/main/java/vender/timevender/com/timevender/LauncherActivity.kt
@@ -0,0 +1,12 @@
+package vender.timevender.com.timevender
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+}
